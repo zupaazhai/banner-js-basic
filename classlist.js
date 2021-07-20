fc9f660861f3ab23df622c710f6a884438531559
Supachai Wongmoon <supachai@outlook.com>
2021-07-20 18:23:05 +0700

    move from readme

---
+++ classlist.js
@@ -0,0 +1,14 @@
+var box = document.querySelector('#box')
+
+// Add class name
+box.classList.add('bg-red')
+// <div id="box" class="bg-red"></div>
+
+// Remove class name
+box.classList.remove('bg-red')
+// <div id="box"></div>
+
+// Check class name exists
+if (box.classList.contains('bg-red')) {
+  ....
+}
