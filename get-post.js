0c2e3415ad60d7bd611596107873bb3a9ea96380
Supachai Wongmoon <supachai@outlook.com>
2021-07-20 18:24:21 +0700

    move from readme

---
+++ get-post.js
@@ -0,0 +1,36 @@
+// GET Old school way
+var req = new XMLHttpRequest()
+req.open('GET', '/my/url', true);
+req.onload = function () {
+  if (this.status >= 200 && this.status < 400) {
+    // Success!
+    var data = JSON.parse(this.response);
+  } else {
+    // We reached our target server, but it returned an error
+
+  }
+}
+req.send()
+
+// GET Fetch API way
+fetch('http://example.com/movies.json')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+
+// POST Old school way
+var request = new XMLHttpRequest();
+request.open('POST', '/my/url', true);
+request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+request.send(data);
+
+// POST Fetch API way
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'post'
+})
+.then(function (res) {
+  return res.json()
+})
+.then(function (data) {
+  console.log(data)
+})
