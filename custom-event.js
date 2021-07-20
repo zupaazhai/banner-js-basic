a9160230f75fb222aa0e07979c24b68e262b93ea
Supachai Wongmoon <supachai@outlook.com>
2021-07-20 18:21:59 +0700

    move from readme

---
+++ custom-event.js
@@ -0,0 +1,13 @@
+// Create custom event
+var evt = new CustomEvent('launch-rocket', {
+  detail: {
+    pilots: []
+  }
+})
+window.dispatchEvent(evt)
+
+// Listen custom event
+window.addEventListener('launch-rocket', function (e) {
+  console.log('Display pilot name')
+  console.log(e.detail)
+})
